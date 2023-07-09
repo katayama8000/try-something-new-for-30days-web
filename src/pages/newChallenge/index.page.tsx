a83d0b4fc37975dacb39391763ea0b357850bfb5
katayama8000 <tattu.0310@gmail.com>
2023-07-09 18:42:58 +0900

    list

--- src/pages/newChallenge/index.page.tsx
+++ src/pages/newChallenge/index.page.tsx
@@ -3,6 +3,11 @@ import { DefaultTemplate } from '@templates/defaultTemplate';
 import { NextPage } from 'next';
 import { Title } from '@mantine/core';
 
+// 必要な項目
+// タイトル
+// 開始日
+// 目的
+
 const NewChallenge: NextPage = () => {
   return (
     <DefaultTemplate>
