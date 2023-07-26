ea62c1d732d45cec2cc4938270567b3a8df06e7f
katayama8000 <tattu.0310@gmail.com>
2023-07-27 00:44:29 +0900

    delete

--- src/state/user.state.ts
+++ src/state/user.state.ts
@@ -2,15 +2,3 @@ import type { User } from 'firebase/auth';
 import { atom } from 'jotai';
 
 export const userAtom = atom<User | null>(null);
-
-// customClaiimsが変更されても動かない
-export const isAdminAtom = atom<Promise<boolean>>(async (get) => {
-  const user = get(userAtom);
-  if (!user) return false;
-  console.log('worked!!!!!!!!!!!!!!!');
-  const idTokenResult = await user.getIdTokenResult();
-  await user.getIdToken(true);
-  await user.reload();
-  console.log({ idTokenResult });
-  return !!idTokenResult.claims.admin;
-});
